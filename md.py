db334124ed50ddb5e0e076eb1c1eaf1b88c72138
okamurayasuyuki <haradashinya@gmail.com>
2012-12-13 19:05:53 +0900

    init

---
+++ md.py
@@ -0,0 +1,14 @@
+__author__ = 'haradashinya'
+import codecs
+import markdown
+
+#input_file = codecs.open("doc.txt",mode="r",encoding="utf8")
+#text = input_file.read()
+#html = markdown.markdown(text,["fenced_code"])
+
+
+
+#print html
+
+
+
